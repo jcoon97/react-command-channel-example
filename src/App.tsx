0c5ad3cbc7a17cc459ffef5a629f835d59fed227
James Coon <james.coon@zoom.us>
2024-05-08 09:54:30 -0600

    Add command channel example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import VideoSDK from "@zoom/videosdk";
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  useEffect(() => {
+    (async () => {
+      const client = VideoSDK.createClient();
+      await client.init("en-US", "Global", { patchJsMedia: true });
+      console.log(client.getCommandClient());
+    })();
+  }, []);
+
+  return <div>Hello, World!</div>;
 }
-
-export default App;
